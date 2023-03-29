70bea47f3f00bca830c926d053e1eabb84e12a39
Lucia Ferencik <lferencikova@gmail.com>
2023-03-29 16:04:17 +0000

    Commit all

---
+++ sql-expression.py
@@ -0,0 +1,42 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# executing the instructions from our localhost "chinook" db
+
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db)
+
+# making the connection
+
+# create variable for "Artist" table
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+# create variable for "Album" table
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+track_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
+    Column("UnitPrice", Float)
+
+)
+
+with db.connect() as connection:
