d53c016c9175ed19b4b1606a054faa53d1210801
Lucia Ferencik <lferencikova@gmail.com>
2023-03-30 09:23:52 +0000

    Sql Alchemy queries

--- sql-expression.py
+++ sql-expression.py
@@ -25,6 +25,7 @@ album_table = Table(
     Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
 )
 
+# create variable for "Track" table
 track_table = Table(
     "Track", meta,
     Column("TrackId", Integer, primary_key=True),
@@ -36,7 +37,31 @@ track_table = Table(
     Column("Milliseconds", Integer),
     Column("Bytes", Integer),
     Column("UnitPrice", Float)
-
 )
 
 with db.connect() as connection:
+
+    # Query #1 - select all records from the "Artist" table
+    # select_query = artist_table.select()
+
+    # Query #2 - select only the "Name" column from the "Artist" table
+    select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+
+    # Query 3 -select only "Queen" from the "Artist" table
+    select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+
+    # Query 4 -select only "ArtistId" from the "Artist" table
+    select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5 -select only albums with "ArtistId" #51 from the "Album" table
+    select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 -select all tracks where the composer is "Queen" from the "Track"
+    # table
+
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
+
